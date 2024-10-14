24252d7fb488fdf747758568501e6d81e00d21e1
Gestalt337 <haoyuan.shi322@outlook.com>
2024-10-14 12:38:22 -0400

    2024/10/14/12:38

--- force.cpp
+++ force.cpp
@@ -2,156 +2,175 @@
 
 double kv=10., ka=1., V0=1., s0=5.4, gm=1.;
 
-void compute_force_v(Collection& C){
+void compute_force(Collection& C){
 
     for (Cell& cl:C.cells_){
         double V = cl.get_volume();
         double P = 2.*kv*(V-V0);
+        double A = cl.get_area();
+        double T = 2.*ka*(A-s0);
         int lenv =cl.vertices_.size();
         for (Vertex* vtx:cl.vertices_){
             double dvdx=0., dvdy=0., dvdz=0.;
-            //for (int ip:vtx->face_ids_[cl.id_]){
-            for (Polygon pp:cl.polygons_){
-                Polygon* poly = &pp;
-                double Rx = poly->center_[0]-cl.center_[0];
-                double Ry = poly->center_[1]-cl.center_[1];
-                double Rz = poly->center_[2]-cl.center_[2];
-                int lenp = size(poly->vertices_);
+            double dadx=0., dady=0., dadz=0.;
+            for (Polygon &poly:cl.polygons_){
+                double Rx = poly.center_[0]-cl.center_[0];
+                double Ry = poly.center_[1]-cl.center_[1];
+                double Rz = poly.center_[2]-cl.center_[2];
+                int lenp = size(poly.vertices_);
                 double M= 1./lenp - 1./lenv;
-                //for (int ip=0;ip<vtx->face_ids_[cl.id_].size();++ip){
-                    //if (vtx->face_ids_[cl.id_][ip]==poly->id_){
 
                 for (int i=0;i<lenp;++i){
-                    double xim1 = poly->vertices_[(i-1+lenp)%lenp]->pos_[0];
-                    double yim1 = poly->vertices_[(i-1+lenp)%lenp]->pos_[1];
-                    double zim1 = poly->vertices_[(i-1+lenp)%lenp]->pos_[2];
-                    double xi = poly->vertices_[i]->pos_[0];
-                    double yi = poly->vertices_[i]->pos_[1];
-                    double zi = poly->vertices_[i]->pos_[2];
-                    double xip1 = poly->vertices_[(i+1+lenp)%lenp]->pos_[0];
-                    double yip1 = poly->vertices_[(i+1+lenp)%lenp]->pos_[1];
-                    double zip1 = poly->vertices_[(i+1+lenp)%lenp]->pos_[2];
-                    double xip = poly->vertices_[i]->pos_[0]-cl.center_[0];
-                    double yip = poly->vertices_[i]->pos_[1]-cl.center_[1];
-                    double zip = poly->vertices_[i]->pos_[2]-cl.center_[2];
-                    double xip1p = poly->vertices_[(i+1+lenp)%lenp]->pos_[0]-cl.center_[0];
-                    double yip1p = poly->vertices_[(i+1+lenp)%lenp]->pos_[1]-cl.center_[1];
-                    double zip1p = poly->vertices_[(i+1+lenp)%lenp]->pos_[2]-cl.center_[2];
-                    if(find(vtx->face_ids_[cl.id_].begin(), vtx->face_ids_[cl.id_].end(), poly->id_) != vtx->face_ids_[cl.id_].end()){
-                        if (poly->vertices_[i]==vtx){
-                            dvdx=dvdx+(Ry*(zim1-zip1)+Rz*(yip1-yim1));
-                            dvdy=dvdy-(Rx*(zim1-zip1)+Rz*(xip1-xim1));
-                            dvdz=dvdz+(Rx*(yim1-yip1)+Ry*(xip1-xim1));
-                        }
+                    double xim1 = poly.vertices_[(i-1+lenp)%lenp]->pos_[0];
+                    double yim1 = poly.vertices_[(i-1+lenp)%lenp]->pos_[1];
+                    double zim1 = poly.vertices_[(i-1+lenp)%lenp]->pos_[2];
+                    double xi = poly.vertices_[i]->pos_[0];
+                    double yi = poly.vertices_[i]->pos_[1];
+                    double zi = poly.vertices_[i]->pos_[2];
+                    double xip1 = poly.vertices_[(i+1)%lenp]->pos_[0];
+                    double yip1 = poly.vertices_[(i+1)%lenp]->pos_[1];
+                    double zip1 = poly.vertices_[(i+1)%lenp]->pos_[2];
+                    double xip = poly.vertices_[i]->pos_[0]-cl.center_[0];
+                    double yip = poly.vertices_[i]->pos_[1]-cl.center_[1];
+                    double zip = poly.vertices_[i]->pos_[2]-cl.center_[2];
+                    double xip1p = poly.vertices_[(i+1)%lenp]->pos_[0]-cl.center_[0];
+                    double yip1p = poly.vertices_[(i+1)%lenp]->pos_[1]-cl.center_[1];
+                    double zip1p = poly.vertices_[(i+1)%lenp]->pos_[2]-cl.center_[2];
+                    // p and pm are essentially short for prime. But they are subtracted by different lengths.
+                    double xipm = poly.vertices_[i]->pos_[0]-poly.center_[0];
+                    double yipm = poly.vertices_[i]->pos_[1]-poly.center_[1];
+                    double zipm = poly.vertices_[i]->pos_[2]-poly.center_[2];
+                    double xip1pm = poly.vertices_[(i+1)%lenp]->pos_[0]-poly.center_[0];
+                    double yip1pm = poly.vertices_[(i+1)%lenp]->pos_[1]-poly.center_[1];
+                    double zip1pm = poly.vertices_[(i+1)%lenp]->pos_[2]-poly.center_[2];
 
+                    double Si=pow((yipm*zip1pm-zipm*yip1pm),2)+pow((xipm*zip1pm-zipm*xip1pm),2)+pow((xipm*yip1pm-yipm*xip1pm),2);
+
+                    if(find(vtx->face_ids_[cl.id_].begin(), vtx->face_ids_[cl.id_].end(), poly.id_) != vtx->face_ids_[cl.id_].end()){
+                        if (poly.vertices_[i]==vtx){
+                            dvdx+=(Ry*(zim1-zip1)+Rz*(yip1-yim1));
+                            dvdy-=(Rx*(zim1-zip1)+Rz*(xip1-xim1));
+                            dvdz+=(Rx*(yim1-yip1)+Ry*(xip1-xim1));
+
+                            dadx+=1./sqrt(Si) * (xipm*(pow(yip1pm,2)+pow(zip1pm,2))-xip1pm*(yipm*yip1pm+zipm*zip1pm));
+                            dady+=1./sqrt(Si) * (yipm*(pow(zip1pm,2)+pow(xip1pm,2))-yip1pm*(zipm*zip1pm+xipm*xip1pm));
+                            dadz+=1./sqrt(Si) * (zipm*(pow(xip1pm,2)+pow(yip1pm,2))-zip1pm*(xipm*xip1pm+yipm*yip1pm));
+                        }
+                        else if (poly.vertices_[(i+1)%lenp]==vtx){
+
+                            dadx+=1./sqrt(Si) * (xip1pm*(pow(yipm,2)+pow(zipm,2))-xipm*(yipm*yip1pm+zipm*zip1pm));
+                            dady+=1./sqrt(Si) * (yip1pm*(pow(zipm,2)+pow(xipm,2))-yipm*(zipm*zip1pm+xipm*xip1pm));
+                            dadz+=1./sqrt(Si) * (zip1pm*(pow(xipm,2)+pow(yipm,2))-zipm*(xipm*xip1pm+yipm*yip1pm));
+                        }
                     }
                     dvdx+=(M*(yip*zip1p-zip*yip1p)+1./lenv * (Rz*(yi-yip1)-Ry*(zi-zip1)));
                     dvdy+=(M*(zip*xip1p-xip*zip1p)+1./lenv * (Rx*(zi-zip1)-Rz*(xi-xip1)));
                     dvdz+=(M*(xip*yip1p-yip*xip1p)+1./lenv * (Ry*(xi-xip1)-Rx*(yi-yip1)));
+
+                    dadx-=(1./lenp)*(xipm*(pow(yip1pm,2)+pow(zip1pm,2))-(xipm+xip1pm)*(yipm*yip1pm+zipm*zip1pm)+xip1pm*(pow(yipm,2)+pow(zipm,2)));
+                    dady-=(1./lenp)*(yipm*(pow(zip1pm,2)+pow(xip1pm,2))-(yipm+yip1pm)*(zipm*zip1pm+xipm*xip1pm)+yip1pm*(pow(zipm,2)+pow(xipm,2)));
+                    dadz-=(1./lenp)*(zipm*(pow(xip1pm,2)+pow(yip1pm,2))-(zipm+zip1pm)*(xipm*xip1pm+yipm*yip1pm)+zip1pm*(pow(xipm,2)+pow(yipm,2)));
                 }
             }
-            vtx->force_[0] +=-P*dvdx*1./6;
-            vtx->force_[1] +=-P*dvdy*1./6;
-            vtx->force_[2] +=-P*dvdz*1./6;
+            vtx->force_[0] +=-T*dadx*0.5-P*dvdx*1./6;
+            vtx->force_[1] +=-T*dady*0.5-P*dvdy*1./6;
+            vtx->force_[2] +=-T*dadz*0.5-P*dvdz*1./6;
         }
     }
 }
 
-void compute_force_a(Collection& C){
-
-    for (Cell& cl:C.cells_){
-        double A = cl.get_area();
-        double T = 2.*ka*(A-s0);
-    for (Vertex* vtx:cl.vertices_){
-        double dAdx=0., dAdy=0., dAdz=0.;
-        double surf_tx=0.,surf_ty=0.,surf_tz=0.;
-        for (int ip:vtx->face_ids_[cl.id_]){
-            Polygon* poly = cl.get_poly_from_id(ip);
-//            double xcj=poly->center_[0]-cl.center_[0];
-//            double ycj=poly->center_[1]-cl.center_[1];
-//            double zcj=poly->center_[2]-cl.center_[2];
-            int lenp = size(poly->vertices_);
-            for (int k=0;k<lenp;k++){
-                if (poly->vertices_[k]==vtx){
-                    int l = (k+1)%lenp;
-                    int h = (k-1+lenp)%lenp;
-                    Vertex* vk=poly->vertices_[k];
-                    Vertex* vh=poly->vertices_[h];
-                    Vertex* vl=poly->vertices_[l];
-                    double xk=vk->pos_[0]-poly->center_[0];
-                    double yk=vk->pos_[1]-poly->center_[1];
-                    double zk=vk->pos_[2]-poly->center_[2];
-                    double xl=vl->pos_[0]-poly->center_[0];
-                    double yl=vl->pos_[1]-poly->center_[1];
-                    double zl=vl->pos_[2]-poly->center_[2];
-                    double xh=vh->pos_[0]-poly->center_[0];
-                    double yh=vh->pos_[1]-poly->center_[1];
-                    double zh=vh->pos_[2]-poly->center_[2];
-                    double Sk = pow(yk*zl-zk*yl,2)+pow(-xk*zl+zk*xl,2)+pow(xk*yl-yk*xl,2);
-                    double Skm1 = pow(yh*zk-zh*yk,2)+pow(-xh*zk+zh*xk,2)+pow(xh*yk-yh*xk,2);
-                    double dSkm1dxk = -2*xh*yh*yk - 2*xh*zh*zk + 2*xk*pow(yh, 2) + 2*xk*pow(zh, 2);
-                    double dSkm1dyk = 2*pow(xh, 2)*yk - 2*xh*xk*yh - 2*yh*zh*zk + 2*yk*pow(zh, 2);
-                    double dSkm1dzk = 2*pow(xh, 2)*zk - 2*xh*xk*zh + 2*pow(yh, 2)*zk - 2*yh*yk*zh;
-                    double dSkdxk = 2*xk*pow(yl, 2) + 2*xk*pow(zl, 2) - 2*xl*yk*yl - 2*xl*zk*zl;
-                    double dSkdyk = -2*xk*xl*yl + 2*pow(xl, 2)*yk + 2*yk*pow(zl, 2) - 2*yl*zk*zl;
-                    double dSkdzk = -2*xk*xl*zl + 2*pow(xl, 2)*zk - 2*yk*yl*zl + 2*pow(yl, 2)*zk;
-
-                    double sumx = (1./4)*(dSkm1dxk * 1/sqrt(Skm1)+dSkdxk * 1/sqrt(Sk));
-                    double sumy = (1./4)*(dSkm1dyk * 1/sqrt(Skm1)+dSkdyk * 1/sqrt(Sk));
-                    double sumz = (1./4)*(dSkm1dzk * 1/sqrt(Skm1)+dSkdzk * 1/sqrt(Sk));
-                    dAdx += sumx;
-                    dAdy += sumy;
-                    dAdz += sumz;
-                    //surf_tx += gm*sumx*poly->is_wall_;
-                    //surf_ty += gm*sumy*poly->is_wall_;
-                    //surf_tz += gm*sumz*poly->is_wall_;
-
-                }
-            }
-            //cout<<poly->id_<<surf_tx<<surf_ty<<surf_tz<<endl;
-        }
-        vtx->force_[0] +=-T*dAdx;//-surf_tx;
-        vtx->force_[1] +=-T*dAdy;//-surf_ty;
-        vtx->force_[2] +=-T*dAdz;//-surf_tz;
-    }
-}
-}
+// I merged volume and area derivative into one function above,
+// so this is no longer needed.
+//void compute_force_a(Collection& C){
+//
+//    for (Cell& cl:C.cells_){
+//        double A = cl.get_area();
+//        double T = 2.*ka*(A-s0);
+//    for (Vertex* vtx:cl.vertices_){
+//        double dAdx=0., dAdy=0., dAdz=0.;
+//        double surf_tx=0.,surf_ty=0.,surf_tz=0.;
+//        for (int ip:vtx->face_ids_[cl.id_]){
+//            Polygon* poly = cl.get_poly_from_id(ip);
+////            double xcj=poly->center_[0]-cl.center_[0];
+////            double ycj=poly->center_[1]-cl.center_[1];
+////            double zcj=poly->center_[2]-cl.center_[2];
+//            int lenp = size(poly->vertices_);
+//            for (int k=0;k<lenp;k++){
+//                if (poly->vertices_[k]==vtx){
+//                    int l = (k+1)%lenp;
+//                    int h = (k-1+lenp)%lenp;
+//                    Vertex* vk=poly->vertices_[k];
+//                    Vertex* vh=poly->vertices_[h];
+//                    Vertex* vl=poly->vertices_[l];
+//                    double xk=vk->pos_[0]-poly->center_[0];
+//                    double yk=vk->pos_[1]-poly->center_[1];
+//                    double zk=vk->pos_[2]-poly->center_[2];
+//                    double xl=vl->pos_[0]-poly->center_[0];
+//                    double yl=vl->pos_[1]-poly->center_[1];
+//                    double zl=vl->pos_[2]-poly->center_[2];
+//                    double xh=vh->pos_[0]-poly->center_[0];
+//                    double yh=vh->pos_[1]-poly->center_[1];
+//                    double zh=vh->pos_[2]-poly->center_[2];
+//                    double Sk = pow(yk*zl-zk*yl,2)+pow(-xk*zl+zk*xl,2)+pow(xk*yl-yk*xl,2);
+//                    double Skm1 = pow(yh*zk-zh*yk,2)+pow(-xh*zk+zh*xk,2)+pow(xh*yk-yh*xk,2);
+//                    double dSkm1dxk = -2*xh*yh*yk - 2*xh*zh*zk + 2*xk*pow(yh, 2) + 2*xk*pow(zh, 2);
+//                    double dSkm1dyk = 2*pow(xh, 2)*yk - 2*xh*xk*yh - 2*yh*zh*zk + 2*yk*pow(zh, 2);
+//                    double dSkm1dzk = 2*pow(xh, 2)*zk - 2*xh*xk*zh + 2*pow(yh, 2)*zk - 2*yh*yk*zh;
+//                    double dSkdxk = 2*xk*pow(yl, 2) + 2*xk*pow(zl, 2) - 2*xl*yk*yl - 2*xl*zk*zl;
+//                    double dSkdyk = -2*xk*xl*yl + 2*pow(xl, 2)*yk + 2*yk*pow(zl, 2) - 2*yl*zk*zl;
+//                    double dSkdzk = -2*xk*xl*zl + 2*pow(xl, 2)*zk - 2*yk*yl*zl + 2*pow(yl, 2)*zk;
+//
+//                    double sumx = (1./4)*(dSkm1dxk * 1/sqrt(Skm1)+dSkdxk * 1/sqrt(Sk));
+//                    double sumy = (1./4)*(dSkm1dyk * 1/sqrt(Skm1)+dSkdyk * 1/sqrt(Sk));
+//                    double sumz = (1./4)*(dSkm1dzk * 1/sqrt(Skm1)+dSkdzk * 1/sqrt(Sk));
+//                    dAdx += sumx;
+//                    dAdy += sumy;
+//                    dAdz += sumz;
+//                    //surf_tx += gm*sumx*poly->is_wall_;
+//                    //surf_ty += gm*sumy*poly->is_wall_;
+//                    //surf_tz += gm*sumz*poly->is_wall_;
+//
+//                }
+//            }
+//            //cout<<poly->id_<<surf_tx<<surf_ty<<surf_tz<<endl;
+//        }
+//        vtx->force_[0] +=-T*dAdx;//-surf_tx;
+//        vtx->force_[1] +=-T*dAdy;//-surf_ty;
+//        vtx->force_[2] +=-T*dAdz;//-surf_tz;
+//    }
+//}
+//}
 
 void dump_data(Collection& coll){
 
     int v_num = 0;
     int p_num = 0;
+    string wrt="";
+    wrt.append("# vtk DataFile Version 3.0\npolydata\nASCII\nDATASET POLYDATA\n");
+    wrt.append("POINTS " + to_string(coll.vertices_.size())+" double\n");
+    for(Vertex& v: coll.vertices_){
+        wrt.append(to_string(v.pos_[0])+" "+to_string(v.pos_[1])+" "+to_string(v.pos_[2])+"\n");
+    }
+    wrt.append("\n");
+    string to_find = "PLACEHOLDER";
+    wrt.append(to_find);
+    //s+="POLYGONS "+to_string(p_num)+" "+to_string(p_num+v_num)+"\n";
     for(Cell& c:coll.cells_){
         p_num+=c.polygons_.size();
         for(Polygon& poly:c.polygons_){
+            wrt.append(to_string(poly.vertices_.size())+" ");
             for(Vertex* v:poly.vertices_){
+                wrt.append(to_string(v->id_)+" ");
                 v_num+=1;
             }
+            wrt.append("\n");
         }
     }
-
-ofstream outFile("data.vtk");
-    outFile<<"# vtk DataFile Version 3.0\npolydata\nASCII\nDATASET POLYDATA\n";
-    outFile<<"POINTS " + to_string(coll.vertices_.size())+" double\n";
-    for(Vertex& v: coll.vertices_){
-        outFile<<to_string(v.pos_[0])+" "+to_string(v.pos_[1])+" "+to_string(v.pos_[2])+"\n";
-    }
-    outFile<<"\n";
-    outFile<<"POLYGONS "+to_string(p_num)+" "+to_string(p_num+v_num)+"\n";
-    for(Cell& c:coll.cells_){
-        p_num+=c.polygons_.size();
-        for(Polygon& poly:c.polygons_){
-            outFile<<to_string(poly.vertices_.size())+" ";
-            for(Vertex* v:poly.vertices_){
-                outFile<<to_string(v->id_)+" ";
-            }
-            outFile<<"\n";
-
-        }
-    }
+    size_t pos = wrt.find(to_find);
+    wrt.replace(pos, to_find.size(), "POLYGONS "+to_string(p_num)+" "+to_string(p_num+v_num)+"\n");
+    ofstream outFile("data.vtk");
+    outFile<<wrt;
     outFile.close();
-
-
 }
 
 
@@ -159,12 +178,11 @@ ofstream outFile("data.vtk");
 int main(){
 
     Collection C("data/vertices.csv","data/cell.csv","data/polygon.csv");
-    compute_force_v(C);
-    //compute_force_a(C);
-    //for (auto v:C.vertices_){cout<<v.force_[0]<<","<<v.force_[1]<<","<<v.force_[2]<<endl;}
+    compute_force(C);
+    for (auto v:C.vertices_){cout<<v.force_[0]<<","<<v.force_[1]<<","<<v.force_[2]<<endl;}
     //cout<<C.cells_[0].volume_<<endl;
-    //dump_data(C);
-    //2024/10/06
+    dump_data(C);
+    //2024/10/14
 
 
     return 0;
